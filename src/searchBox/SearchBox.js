0b374121f4e22dad771d5cd44751d8320bc4d5e3
Klaudia Budzyńska <klaudia.budzynska1@gmail.com>
2020-03-14 17:44:32 +0100

    Search box design

--- src/searchBox/SearchBox.js
+++ src/searchBox/SearchBox.js
@@ -1,18 +1,25 @@
 import React, { useState } from 'react';
 
+import './searchBox.css';
+
 function SearchBox({ handleSearch }) {
   const [ value, setValue ] = useState('');
 
   return (
-    <input 
-      type="text"
-      value={value}
-      onChange={e => {
-          setValue(e.target.value);
-          handleSearch(e.target.value);
+    <div className="search-box">
+      <input 
+        className="search-box__input"
+        type="text"
+        placeholder="I'm looking for.."
+        value={value}
+        onChange={e => {
+            setValue(e.target.value);
+            handleSearch(e.target.value);
+          }
         }
-      }
-    />
+      />
+      <label className="search-box__label">Search</label> 
+    </div>
   );
 }
 
